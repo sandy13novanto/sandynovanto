469b670bb24cffd8054d62e8f60dfa46ee97eb7a
sandy13novanto <52731767+sandy13novanto@users.noreply.github.com>
2019-07-10 12:01:00 +0700

    Add files via upload

---
+++ struktur data.py
@@ -0,0 +1,12 @@
+list = [1,4,2,5,3,8,6,9,7,11,10]
+
+print(*mengecek List awal*)
+print(list)
+
+print(*menambahkan Data Listdengan Insert*)
+list.insert(2,70)
+print(list)
+
+print(*Mengurutkan List*)
+list.sort()
+print(list)
